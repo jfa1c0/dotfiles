e8fd050b8bb0f1212efebdb3352343bc8b276ff1
Joseph <joseph@falconite.io>
2023-06-01 15:18:25 -0500

    chore(zsh): clean up various scattered configs

--- .config/tmux/install_plugins.sh
+++
@@ -1,7 +0,0 @@
-printf "Install TPM plugins\n"
-tmux new -d -s __noop >/dev/null 2>&1 || true
-tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.config/tmux/plugins"
-"$HOME"/.config/tmux/plugins/tpm/bin/install_plugins || true
-tmux kill-session -t __noop >/dev/null 2>&1 || true
-
-printf "OK: Completed\n"
